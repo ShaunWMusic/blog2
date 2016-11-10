1418114a62228b32350ca2090425119a3d3b2cdb
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-10 11:47:13 -0600

    I can see list of posts from vue

--- resources/javascript/routes/application.vue
+++ resources/javascript/routes/application.vue
@@ -1,6 +1,10 @@
 <template lang="html">
   <div class="">
-    <router-view></router-view>
+    <router-view
+      :posts="posts"
+      @findAllPosts="findAllPosts"
+      @findPost="findPost"
+    ></router-view>
     <h2>Appy</h2>
   </div>
 </template>
@@ -9,11 +13,28 @@
 export default {
   data() {
     return {
+      posts: [],
     };
   },
 
-  methods: {
+  mounted() {
+    this.findAllPosts();
+  },
 
+  methods: {
+    findAllPosts() {
+      fetch('/api/posts', {
+        credentials: 'same-origin',
+      })
+        .then((r) => r.json())
+        .then((posts) => {
+          this.posts = posts;
+        });
+    },
+
+    findPost() {
+
+    },
   },
 };
 </script>
--- resources/javascript/routes/index.vue
+++ resources/javascript/routes/index.vue
@@ -1,11 +1,18 @@
 <template lang="html">
   <div class="">
     <h2>IS THIS WORKING!!!</h2>
+
+    <ul>
+      <li v-for="post in posts">
+        {{ post.title }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
+  props: ['posts'],
   data() {
     return {
     };
