ad2af04873afba1d96e557c89f533b04713fc0d0
Ryan Tablada <ryan.tablada@gmail.com>
2016-11-10 11:08:32 -0600

    Add vue

---
+++ resources/javascript/routes/application.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="">
+    <router-view></router-view>
+    <h2>Appy</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
---
+++ resources/javascript/routes/index.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div class="">
+    <h2>IS THIS WORKING!!!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
